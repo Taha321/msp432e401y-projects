de89862b2531eb3a8182746888abf1d64132b6ad
tsaqib <tsaqib@devenv.myguest.virtualbox.org>
2025-10-19 17:45:16 -0400

    updated uart_test to echo back user input

--- uart_test/main.c
+++ uart_test/main.c
@@ -9,9 +9,47 @@ void wait() {
     }
 }
 
+static int pn_led_state = 0;
+void led_init(void) {
+    reg_sysctl->RCGCGPIO |= 1 << 12;
+
+    __asm("nop");
+    __asm("nop");
+    __asm("nop"); 
+
+    reg_gpio_pn->DIR |= 0xff;       // set port N as output
+    reg_gpio_pn->AFSEL &= ~0xff;    // set afsel for port N
+    reg_gpio_pn->PC &= ~0xff;       // gpiopc port N
+    reg_gpio_pn->DR2R |= 0xff;      // 2ma drive on port N
+    reg_gpio_pn->DR4R &= ~0xff;     // disable 4ma on port N
+    reg_gpio_pn->DR8R &= ~0xff;     // disable 8ma on port N
+    reg_gpio_pn->DEN |= 0xff;       // enable digital on port N
+}
+
+void UART0_IRQHandler(void)
+{
+    uint32_t recieved_char = reg_uart0->TDR;
+
+    if(recieved_char == '\r') {
+        recieved_char = '\n';
+        reg_uart0->TDR = recieved_char; // echo back received character
+        while ((reg_uart0->FR >> 3) & 1) {} // wait until tx is completed
+        reg_uart0->TDR = '\r'; // send carriage return after newline
+        while ((reg_uart0->FR >> 3) & 1) {} // wait until tx is completed
+        goto exit;
+    }
+    reg_uart0->TDR = recieved_char; // echo back received character
+    while ((reg_uart0->FR >> 3) & 1) {} // wait until tx is completed
+
+exit:
+    reg_uart0->ICR = 0x10; // clear rx interrupt
+}
 
 int main(void)
 {
+
+    led_init();
+
     reg_sysctl->RCGCGPIO |= 0xffff;
     reg_sysctl->RCGCUART |= 0x1; // enable clock to UART0
 
@@ -33,14 +71,20 @@ int main(void)
     reg_uart0->CTL &= ~0x1; // disable uart0 while configuring
     reg_uart0->IBRD = 8; // uart baud rate 115200, int 8
     reg_uart0->FBRD = 44; // uart baud rate 115200, frac 44
-    reg_uart0->LCRH = 0x70; // 8 bit, no parity, one stop bit, fifo enabled
+    reg_uart0->LCRH = 0x60; // 8 bit, no parity, one stop bit, fifo enabled
+    reg_uart0->IM |= 0x10; // enable rx interrupt
+
+    HWREG(0xE000E100) |= 0x1 << 5; // enable interrupt number 5 (UART0)
+
     reg_uart0->CTL |= 0x01; // uart enable
     reg_uart0->CTL |= 0x300; // uart tx and rx enabl
 
 
     while(1)
     {   
-        reg_uart0->TDR = 'A'; // send character 'A' over UART
+        reg_gpio_pn->DATA1 = 1;
+        wait();
+        reg_gpio_pn->DATA1 = 0;
         wait();
     }
 
